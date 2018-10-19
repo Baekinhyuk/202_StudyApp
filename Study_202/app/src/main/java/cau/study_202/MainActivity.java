e5b81bf3b7a39b9e0553467a3751d5f4fb7914d8
Baekinhyuk <bih94@hanmail.net>
2018-10-19 19:12:53 +0900

    로그인시 리더인지 아닌지 판단하는 부분 추가

--- Study_202/app/src/main/java/cau/study_202/MainActivity.java
+++ Study_202/app/src/main/java/cau/study_202/MainActivity.java
@@ -3,10 +3,15 @@ package cau.study_202;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.MalformedURLException;
 import cau.study_202.network.NetworkUtil;
@@ -45,7 +50,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 try {
                     Phprequest request = new Phprequest(Phprequest.BASE_URL +"login.php");
-
                     String result = request.login(String.valueOf(editID.getText()),String.valueOf(editPassword.getText()));
                     if(result.equals("1")){
                         Toast.makeText(getApplication(),"비밀번호가 일치하지 않습니다.",Toast.LENGTH_SHORT).show();
@@ -54,15 +58,15 @@ public class MainActivity extends AppCompatActivity {
                         Toast.makeText(getApplication(),"존재하지 않는 아이디 입니다.",Toast.LENGTH_SHORT).show();
                     }
                     else {
-                        /* 여기도 php 통신해서 nickname, groupid 가져와서 Loginstatus에 저장해야함*/
+                        /* 여기도 php 통신해서 leaderid, groupid 가져와서 Loginstatus에 저장 해야함 로그인 정보를 통해서 판단*/
                         LoginStatus.setMemberID(String.valueOf(editID.getText()));
                         try{
-                            LoginStatus.setGroupID(Integer.parseInt(result));
+                            inputloginstatus(result,String.valueOf(editID.getText()));
                         }catch (IllegalArgumentException e) {
                             LoginStatus.setGroupID(-1);
                         }
                         /*groupID 제대로 찍히는지 test*/
-                        //Toast.makeText(getApplication(),String.valueOf(LoginStatus.getGroupID()),Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(getApplication(),String.valueOf(LoginStatus.isLeader()),Toast.LENGTH_SHORT).show();
                         Intent i = new Intent(MainActivity.this,
                                 HomeActivity.class);
                         startActivity(i);
@@ -85,5 +89,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+
+    private void inputloginstatus(String result,String ID) {
+
+        String TAG_JSON = "loginstatus";
+        String TAG_LEADERID = "leaderid";
+        String TAG_GROUPID = "groupid";
+
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            JSONObject item = jsonArray.getJSONObject(0);
+            String leaderid = item.getString(TAG_LEADERID);
+            String groupid = item.getString(TAG_GROUPID);
+            LoginStatus.setGroupID(Integer.parseInt(groupid));
+            if (leaderid.equals(ID)) {
+                LoginStatus.setLeader();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
