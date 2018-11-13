5ba9e72a46980da86a00385c8809ca402203c103
ChanheeBaek <chb2561@gmail.com>
2018-11-13 12:44:57 +0900

    출석 시 벌금 차감 구현

--- php_test/create_image_board.php
+++ php_test/create_image_board.php
@@ -10,10 +10,28 @@ $_POST['image_path']
 $_POST['image_name']
 $_POST['state']
 */
-$ImageData = $_POST['image_path'];
-$data_stream = "'".$_POST['memberID']."',0,'".$_POST['groupID']."',".$_POST['state'];
-$query = "insert into study202.attendence (memberID, method, groupID, state) VALUES (".$data_stream.")";
+
+// 지각 벌금 값 가져오기
+$query = "select latefine from study202.group where ID="."'".$_POST['groupID']."'";
 $result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
+
+if($_POST['state'] == 1){ // 지각이라면 멤버 벌금 update (신뢰도 추가해야함)
+  $query = "update study202.member set fine = fine + ".$row['latefine']." where ID ="."'".$_POST['memberID']."'";
+  $result = mysqli_query($conn, $query);
+}
+
+// 멤버 벌금 값 가져오기 (신뢰도 추가해야함)
+$query = "select fine from study202.member where ID ="."'".$_POST['memberID']."'";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
+// 출석하기  method 0 : 사진 , 1 : 비콘 , 2 : gps
+$data_stream = "'".$_POST['memberID']."',0,'".$_POST['groupID']."',".$_POST['state'].",".$row['fine'];
+$query = "insert into study202.attendence (memberID, method, groupID, state, fine) VALUES (".$data_stream.")";
+$result = mysqli_query($conn, $query);
+
+
+$ImageData = $_POST['image_path'];
 
 $ImagePath = "images/".$_POST['image_name'].$_POST['memberID'].".jpg";
 $ServerURL = "https://cauteam202.com/$ImagePath";
